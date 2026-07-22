c8bca2a231e319c4b286a3b80fddd211cd2db7dc
Vinayak Mishra <vinayakmishra2805@gmail.com>
2026-07-22 19:29:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Palindrome String/palindrome-string.cpp
+++ Difficulty: Easy/Palindrome String/palindrome-string.cpp
@@ -1,10 +1,15 @@
 class Solution {
   public:
+  void rev(string &s,string &ans,int i,int j){
+      if(i>j){return ;}
+      ans[i]=s[j];
+      rev(s,ans,i+1,j-1);
+  }
     bool isPalindrome(string& s) {
-        string x=s;
-        reverse(s.begin()+0,s.begin()+s.size());
-        if(x==s){return true;}
+        string ans=s;
+        int i=0,j=s.size()-1;
+        rev(s,ans,i,j);
+        if(ans==s){return true;}
         else{return false;}
-        
     }
 };
